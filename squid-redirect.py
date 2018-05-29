74bb0083234d6c60f7d45aea012fb94e399ec1da
Andrey Sorokin <sorokinac@gmail.com>
2018-05-29 18:05:35 +0500

    Second commit

--- squid-redirect.py
+++ squid-redirect.py
@@ -13,6 +13,7 @@ import re
 
 #http://yandex.ru 172.17.8.175/saini.co.in - GET -
 #([a-z]\w+.[a-z]{2,6})
+
 #--------------------------------------------------
 def url_clear(url):
     '''Очистка url адреса'''
@@ -32,28 +33,37 @@ def loadingDataJSON():
 
     return dictValJOSN
 #------------------------------------------------
-def rewrite(url, ch_id):
+def rewrite(url, ch_id, myip, myport):
     dictValJSON = loadingDataJSON()
     url = url_clear(url)
     for lineURL in dictValJSON.keys():
         if url.startswith(lineURL) :
-            rewriteUrl = ch_id + ' OK status=301 url=https://' + dictValJSON[lineURL] + '\n'
-            #print(rewriteUrl)
+            rewriteUrl = ch_id + ' OK status=301 url=https://' + dictValJSON[lineURL] + ' ' + myip + ' ' + myport + '\n'
+            syslog.syslog(6, 'Squid_redirect from ' + url + ' to ' + dictValJSON[lineURL])
+            #my_logger.debug('this is debug')
+            print(rewriteUrl)
             break
+    syslog.syslog(6, 'Squid_request_URL: ' + url)
+    rewriteUrl = ch_id + ' ERR ' + myip + ' ' + myport + '\n'
+    #print('rgequst')
+
+
     return rewriteUrl
 
 #-------------------------------------------------
 def main():
     while True:
         inRequest = sys.stdin.readline()
-        [ch_id, url, ipaddr, method, user] = inRequest.split()
-        outRequest = rewrite(url, ch_id)
+        try:
+            [ch_id, url, ipaddr, ident, method, myip, myport] = inRequest.split()
+        except ValueError:
+            syslog.syslog(3, 'ValueError inRequest.split()')
+            #print('error')
+            continue
+        outRequest = rewrite(url, ch_id, myip, myport)
         sys.stdout.write(outRequest)
         sys.stdout.flush()
 
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
-
-
-
